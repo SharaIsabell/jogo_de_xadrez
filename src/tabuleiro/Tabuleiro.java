a9bf2f78065955d490cf84de1c1e1ec93352fa41
Shara Isabell <isabeloliveira425@gmail.com>
2024-03-29 23:02:03 -0300

    TabuleiroException e atualizando a classe Tabuleiro para uma programação mais defensiva

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -6,38 +6,59 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 	
 	public Tabuleiro(int linhas, int colunas) {
+		if(linhas < 1 || colunas < 1) {
+			throw new TabuleiroException("Erro criando tabuleiro: deve existir pelo menos uma linha e uma coluna");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
 	}
 	
-	public Peca peca(int linha, int coluna) {
+	public Peca peca(int linha, int coluna) { //retorna a peça que está na posição dada
+		if(!posicaoExiste(linha, coluna)) {
+			throw new TabuleiroException("Posição não existe no tabuleiro");
+		}
 		return pecas[linha][coluna];
 	}
 	
-	public Peca peca(Posicao posicao) {
+	public Peca peca(Posicao posicao) { //retorna a peça que está na posição dada
+		if(!posicaoExiste(posicao)) {
+			throw new TabuleiroException("Posição não existe no tabuleiro");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
-	public void colocarPeca(Peca peca, Posicao posicao) {
+	public void colocarPeca(Peca peca, Posicao posicao) { //coloca a peça dada na posição também dada
+		if(temUmaPeca(posicao)) {
+			throw new TabuleiroException("Já existe uma peça na posição " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
 	}
-
+	
+	private boolean posicaoExiste(int linha, int coluna) { //retorna se a posição existe no tabuleiro
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+	
+	public boolean posicaoExiste(Posicao posicao) { //retorna se a posição existe no tabuleiro
+		return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+	}
+	
+	public boolean temUmaPeca(Posicao posicao) { //retorna se na posição dada há uma peça
+		if(!posicaoExiste(posicao)) {
+			throw new TabuleiroException("Posição não existe no tabuleiro");
+		}
+		return peca(posicao) != null;
+	}
+	
+	//Getters and setters
+	
 	public int getLinhas() {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
-	}
-
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
 	}
 
 }
