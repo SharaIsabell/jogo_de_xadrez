ec65c30312e9b80f51d5a43fe537c69ef38e2c6e
Shara Isabell <isabeloliveira425@gmail.com>
2024-04-07 16:13:31 -0300

    Limpando a tela e tratando exceções

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -1,10 +1,12 @@
 package aplicacao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoXadrez;
+import xadrez.XadrezException;
 
 public class Main {
 	public static void main(String[] args) {
@@ -13,18 +15,30 @@ public class Main {
 		PartidaXadrez partida = new PartidaXadrez();
 		
 		while(true) {
-			UI.printTabuleiro(partida.getPecas());
-			
-			System.out.println();
-			System.out.print("Inicial: ");
-			PosicaoXadrez inicial = UI.lerPosicaoXadrez(leitor);
-			
-			System.out.println();
-			System.out.print("Desejada: ");
-			PosicaoXadrez desejada = UI.lerPosicaoXadrez(leitor);
-			
-			PecaXadrez pecaCapturada = partida.moverPeca(inicial, desejada);
-			System.out.println();
+			try {
+				UI.clearScreen(); // limpando a tela toda vez que encerrar a jogada para não ficar muito poluído
+				
+				UI.printTabuleiro(partida.getPecas());
+				
+				System.out.println();
+				System.out.print("Inicial: ");
+				PosicaoXadrez inicial = UI.lerPosicaoXadrez(leitor);
+				
+				System.out.println();
+				System.out.print("Desejada: ");
+				PosicaoXadrez desejada = UI.lerPosicaoXadrez(leitor);
+				
+				PecaXadrez pecaCapturada = partida.moverPeca(inicial, desejada);
+				System.out.println();
+			}
+			catch(XadrezException e) {
+				System.out.println(e.getMessage());
+				leitor.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				leitor.nextLine();
+			}
 		}
 	}
 }
--- src/aplicacao/UI.java
+++ src/aplicacao/UI.java
@@ -3,8 +3,6 @@ package aplicacao;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import javax.management.RuntimeErrorException;
-
 import xadrez.Cor;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoXadrez;
@@ -36,6 +34,12 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
+	
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}	
 	
 	public static PosicaoXadrez lerPosicaoXadrez(Scanner leitor) { //pegando uma posição dada pelo jogador / Ex.: a1
 		try {
